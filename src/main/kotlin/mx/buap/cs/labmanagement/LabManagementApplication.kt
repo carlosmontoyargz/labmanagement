e4a42380e381db83003b58551471feff3071ce72
Carlos Montoya <carlos.montoya.rdgz@gmai.com>
2022-07-17 17:49:17 -0500

    Commit inicial

---
+++ src/main/kotlin/mx/buap/cs/labmanagement/LabManagementApplication.kt
@@ -0,0 +1,11 @@
+package mx.buap.cs.labmanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LabManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<LabManagementApplication>(*args)
+}
---
+++ src/test/kotlin/mx/buap/cs/labmanagement/LabManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package mx.buap.cs.labmanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LabManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
